cb3ff6a006659069a1f80b4e5159c15fb049fa97
rarxn <dann1331@mail.ru>
2022-12-27 09:47:56 +0400

    6

--- sixth.py
+++ sixth.py
@@ -5,15 +5,9 @@ def set_error(w, kol_vo_oshibok, пакет):
     word = np.copy(w)
     idx = []
     if пакет:
-        buff = np.random.randint(2, size=kol_vo_oshibok)
-        if np.any(buff) == 0:
-            buff[np.random.randint(kol_vo_oshibok)] ^= 1
-        index = np.random.randint(word.size)
-        j = index
-        for i in range(len(buff)):
-            idx.append(j)
-            word[j] ^= buff[i]
-            j = j + 1 if j + 1 < len(word) else 0
+        for i in range(kol_vo_oshibok):
+            idx.append(i)
+            word[i] ^= 1
     else:
         for i in np.random.choice(word.size, kol_vo_oshibok, replace=False):
             idx.append(i)
@@ -35,10 +29,6 @@ def декодирование(w, g, n, t, пакет):
         if (пакет and is_error(s_i, t)) or (not пакет and np.sum(s_i) <= t):
             return np.polyadd(np.polymul(np.eye(1, n - i + 1, dtype=int)[0], s_i) % 2, w) % 2 if i != 0 \
                 else np.polyadd(s_i, w) % 2
-        # else:
-        #     if np.sum(s_i) <= t:
-        #         return np.polyadd(np.polymul(np.eye(1, n - i + 1, dtype=int)[0], s_i) % 2, w) % 2 if i != 0 \
-        #             else np.polyadd(s_i, w) % 2
     return None
 
 
@@ -53,11 +43,25 @@ def исследование(n, k, t, g, errors, пакет):
         print(f"слово с {kol_vo_oshibok} ошибками:\n{word}")
         word = декодирование(word, g, n, t, пакет)
         print(f"исправлено:{np.array_equal(w, word)}, исправленное слово:\n{word}")
+    if пакет:
+        доп_тест(w, g, n, t, пакет)
+
+
+def доп_тест(w, g, n, t, пакет):
+    word = np.copy(w)
+    print(f"\n\t\tКоличество ошибок:{3}")
+    print(f"закодированное слово:\n{word}")
+    idx = [0, 2, 4]
+    print(f'индексы ошибок: {idx}')
+    word[idx] ^= 1
+    print(f"слово с {3} ошибками:\n{word}")
+    word = декодирование(word, g, n, t, пакет)
+    print(f"исправлено:{np.array_equal(w, word)}, исправленное слово:\n{word}")
 
 
 def main():
-    # print(f"\ng(x)=1 + x^2 + x^3")
-    # исследование(7, 4, 1, np.array([1, 1, 0, 1]), 3, False)
+    print(f"\ng(x)=1 + x^2 + x^3")
+    исследование(7, 4, 1, np.array([1, 1, 0, 1]), 3, False)
     # print('====================================')
     print(f"\ng(x)=1 + x^3 + x^4 + x^5 + x^6")
     исследование(15, 9, 3, np.array([1, 1, 1, 1, 0, 0, 1]), 4, True)
